8080d2e9a9c5da464925ab5fa1a444b64d920186
Vin Busquet <computationalcore@gmail.com>
2017-11-13 21:56:00 -0200

    feat: Improve Authentication component

--- src/Authentication.js
+++ src/Authentication.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import { Link } from 'react-router-dom';
 
 /**
  * Authentication Page
@@ -9,23 +10,31 @@ import RaisedButton from 'material-ui/RaisedButton';
  */
 function Authentication(props) {
 	return (
-		<div className="loader-box">
-			<div>
-				<Link to="/login">
+		<div className="app-authentication">
+			<p>Welcome to MyReads!</p>
+			<p>This is a bookshelf app that allow users to select and categorize books they have read, are currently reading, or
+				want to read.</p>
+			<p>This is the final assessment project for the Udacity's React Fundamentals course</p>
+			<div className="auth-button-ext">
+				<Link className="auth-button" to="/login">
 					<RaisedButton
 						label="Login"
 						primary={true}
 					/>
 				</Link>
-			</div>
-			<div>
-				<Link to="/login">
+				<br />
+				<br />
+				<Link className="auth-button" to="/register">
 					<RaisedButton
 						label="Register"
 						primary={true}
 					/>
 				</Link>
 			</div>
+			<p className="important-note">
+				Important: The backend API provided by Udacity eventually delete the accounts.
+				This is just an evaluation project not intended to be use in daily basis.
+			</p>
 		</div>
 	);
 }
