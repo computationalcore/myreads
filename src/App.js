2bc68862f9780c53af374b3cb97717b423a44e1b
Vin Busquet <computationalcore@gmail.com>
2017-11-13 22:23:03 -0200

    refactor: Move BookInfoPage to its own file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
-import AppBar from 'material-ui/AppBar';
-import ContentAdd from 'material-ui/svg-icons/content/add';
-import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
-import SvgIcon from 'material-ui/SvgIcon';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import IconButton from 'material-ui/IconButton';
 import sortBy from 'sort-by';
+import scrollToComponent from 'react-scroll-to-component';
+import AppBar from 'material-ui/AppBar';
+import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import Drawer from 'material-ui/Drawer';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import IconButton from 'material-ui/IconButton';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Subheader from 'material-ui/Subheader';
+import BookInfoPage from './BookInfoPage';
 import * as BooksAPI from './BooksAPI';
 import * as BookUtils from './BookUtils';
-import BookInfo from './BookInfo';
 import Bookshelf from './Bookshelf';
 import Authentication from './Authentication';
 import Login from './Login';
@@ -18,63 +22,6 @@ import Register from './Register';
 import Search from './Search';
 import './App.css';
 
-
-
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import Menu from 'material-ui/Menu';
-
-
-import Subheader from 'material-ui/Subheader';
-
-import scrollToComponent from 'react-scroll-to-component';
-
-/**
- * @description BookInfo page component show the App title bar and BookInfo component into as different page.
- * The function signature is designed to be used as the 'component' prop for the Router component.
- * @param bookId	the id of the book which details to be showed at this page
- * @param state		If true it will show the back button at app bar otherwise home button
- * @returns {XML}
- * @constructor
- */
-const BookInfoPage = ({ history, match: { params: {bookId} }, location: {state} }) => {
-
-	return (
-		<div>
-			<div className="app-bar">
-				<AppBar
-					title={<div className="app-bar-title app-bar-icon">Book Details</div>}
-					iconElementLeft={
-						<IconButton>
-							{state &&
-								<ArrowBack />
-							}
-							{!state &&
-								<SvgIcon>
-									<path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-								</SvgIcon>
-							}
-						</IconButton>
-					}
-					onLeftIconButtonTouchTap={
-						() => {
-							// Go Back
-							if(state)
-								history.goBack();
-							// Go to Home
-							else{
-								history.push("/");
-							}
-						}
-					}
-				/>
-			</div>
-			<div className="app-content">
-				<BookInfo bookId={bookId} />
-			</div>
-		</div>
-	);
-};
 
 class BooksApp extends React.Component {
 
