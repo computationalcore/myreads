b04f3be8e235784d0c86d0bb04b063925db4b43e
Vin Busquet <computationalcore@gmail.com>
2017-11-18 22:15:07 -0200

    feat: Add fork ribbon link to Authentication

--- src/Authentication.js
+++ src/Authentication.js
@@ -10,6 +10,13 @@ import RaisedButton from 'material-ui/RaisedButton';
 function Authentication(props) {
 	return (
 		<div className="app-authentication">
+			<a href="https://github.com/computationalcore/myreads" target="_blank">
+				<img
+					style={{position: 'absolute', top: 0, right: 0, border: 0, zIndex: 9999}}
+					src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67"
+					alt="Fork me on GitHub"
+					data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"/>
+			</a>
 			<div className="app-authentication-internal">
 			<p>Welcome to MyReads!</p>
 			<p>This is a bookshelf app that allow users to select and categorize books they had read, are currently
